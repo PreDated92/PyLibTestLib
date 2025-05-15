fad85d4d7af084775348ab87ad0adfe81c9b8dbd
PreDated92 <PreDated92@users.noreply.github.com>
2025-05-15 23:29:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PyLibTestLib
+ 
---
+++ lktestlib.py
@@ -0,0 +1,9 @@
+import datetime
+
+class lktestlib:
+    def __init__(self):
+        pass
+
+    def get_time_now():
+        now = datetime.datetime.now()
+        return now
